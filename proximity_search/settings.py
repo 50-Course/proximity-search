c32616f8528509c85c7569db17e18731bd80a6bf
50-Course <eridotdev@gmail.com>
2024-04-02 11:14:08 +0100

    feat: add `redis` cache

--- proximity_search/settings.py
+++ proximity_search/settings.py
@@ -86,6 +86,19 @@ DATABASES = {
     }
 }
 
+CACHE_TTL = 60 * 15
+
+CACHE = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.rediscache.RedisCache",
+        "LOCATION": "redis://",
+        "TIMEOUT": CACHE_TTL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
