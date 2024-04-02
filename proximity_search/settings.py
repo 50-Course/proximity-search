f72653abfc805a1673f1ed241116e6ba1cc201cf
50-Course <eridotdev@gmail.com>
2024-04-02 15:19:22 +0100

    feat: whitelist set of IPs for `cors_headers`

--- proximity_search/settings.py
+++ proximity_search/settings.py
@@ -162,3 +162,13 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = False
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8080",
+]
